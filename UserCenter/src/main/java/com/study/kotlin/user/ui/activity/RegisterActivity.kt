03aa7b06d48b6ced918ec3af45946be6d44c821b
yuehongjie <yuehongjie@bailitop.com>
2018-12-25 20:04:27 +0800

    第一次提交 -- 分模块练习

---
+++ UserCenter/src/main/java/com/study/kotlin/user/ui/activity/RegisterActivity.kt
@@ -0,0 +1,21 @@
+package com.study.kotlin.user.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import com.study.kotlin.user.R
+import kotlinx.android.synthetic.main.activity_register.*
+
+class RegisterActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_register)
+
+        mBtnRegister.setOnClickListener {
+            //Toast.makeText(this, "注册", Toast.LENGTH_SHORT).show()
+
+
+        }
+    }
+}
---
+++ UserCenter/src/main/java/com/study/kotlin/user/ui/activity/TestAnkoActivity.kt
@@ -0,0 +1,13 @@
+package com.study.kotlin.user.ui.activity
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import com.study.kotlin.user.R
+
+class TestAnkoActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_test_anko)
+    }
+}
